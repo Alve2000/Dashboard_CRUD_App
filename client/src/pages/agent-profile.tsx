6224e71c69a2a43d03511d09a11fe89c426dca21
Alvin <alvin.nayak2@gmail.com>
2023-02-19 02:45:39 +0530

    CRUD

---
+++ client/src/pages/agent-profile.tsx
@@ -0,0 +1,34 @@
+
+import { useOne } from '@pankod/refine-core'
+import { useParams } from '@pankod/refine-react-router-v6'
+
+import { Profile } from 'components'
+
+const AgentProfile = () => {
+
+  const { id } = useParams()
+  const { data, isLoading, isError } = useOne({
+    resource: 'users',
+    id: id as string,
+  })
+
+  const myProfile = data?.data ?? []
+
+  if (isLoading) return <div>loading...</div>
+  if (isError) return <div>error...</div>
+
+  console.log(myProfile)
+
+  return (
+
+    <Profile
+      type='Agent'
+      name={myProfile.name}
+      email={myProfile.email}
+      avatar={myProfile.avatar}
+      properties={myProfile.allProperties}
+    />
+  )
+}
+
+export default AgentProfile
---
+++ client/src/pages/index.ts
@@ -0,0 +1,22 @@
+
+import Home from './home'
+import Agents from './agent'
+import { Login } from './login'
+import MyProfile from './my-profile'
+import AgentProfile from './agent-profile'
+import EditProperty from './edit-property'
+import AllProperties from './all-properties'
+import CreateProperty from './create-property'
+import PropertyDetails from './property-details'
+
+export {
+    Home,
+    Agents,
+    Login,
+    MyProfile,
+    AgentProfile,
+    EditProperty,
+    AllProperties,
+    CreateProperty,
+    PropertyDetails,
+}

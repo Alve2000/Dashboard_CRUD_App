c4e5e9fe32c4ea8d8828b9634c9280590922d5e5
Alvin Nayak <76550424+Alve2000@users.noreply.github.com>
2023-02-19 03:11:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
-import connectDB from './mongoDB/connect.js'
+import connectDB from './mongodb/connect.js'
 import userRouter from './routes/user.routes.js'
 import propertyRouter from './routes/property.routes.js'
 
